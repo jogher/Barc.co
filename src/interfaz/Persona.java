1d193fb0b0561df9c98ee51703ea1a92cebeee78
jogher <jogher.valdes@davinci.edu.ar>
2023-09-23 18:08:20 -0300

    agregue contructor y metodos a la clase persona

--- src/interfaz/Persona.java
+++ src/interfaz/Persona.java
@@ -1,5 +1,78 @@
 package interfaz;
 
 public class Persona {
+	int id;
+	int dni;
+	String nombre;
+	String apellido;
+	
+	
+	
+	public Persona(int id, int dni, String nombre, String apellido) {
+		super();
+		this.id = id;
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+
+
+	public Persona() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public int getDni() {
+		return dni;
+	}
+
+
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	
+	
 
 }
