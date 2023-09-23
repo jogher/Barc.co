2e4bdb5e760117a43049ded6265c26b958ded910
jogher <jogher.valdes@davinci.edu.ar>
2023-09-23 18:17:14 -0300

    cree clase Cliente

---
+++ src/interfaz/Cliente.java
@@ -0,0 +1,34 @@
+package interfaz;
+
+public class Cliente extends Persona{
+	
+	String telefono;
+	String email;
+	
+	public Cliente(int id, int dni, String nombre, String apellido, String telefono, String email) {
+		super(id, dni, nombre, apellido);
+		this.telefono = telefono;
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+
+}
