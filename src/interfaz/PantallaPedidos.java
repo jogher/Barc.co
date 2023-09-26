dd2d1165257f2f7bd11b6575f2b8c36597c48308
Tomas De Paulo <tomas.depaulo@davinci.edu.ar>
2023-09-26 10:39:43 -0300

    Update PantallaPedidos

--- src/interfaz/PantallaPedidos.java
+++ src/interfaz/PantallaPedidos.java
@@ -1,10 +1,40 @@
 package interfaz;
 
+import javax.swing.JOptionPane;
+
+import logica.Pedido;
+
 public class PantallaPedidos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void Menu(Pedido pedido) {
+		
+		String[] Opciones=
+			{
+					"Realizar pedido","Seguimiento de pedidos","Cancelar pedido","Salir"
+			};
+		int op=0;
+		do {
+			op= JOptionPane.showOptionDialog(null, "Menu Pedidos", null, 0, 0, null, Opciones, Opciones[0]);
+			switch (op) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "Realizar pedido \n"
+						+ "Se debe ingresar(Productos)");
+				break;
+			case 1:
+				JOptionPane.showMessageDialog(null, "Seguimiento de Pedidos \n"
+						+ "Seleccione un pedido");
+				break;
+			case 2:
+				JOptionPane.showMessageDialog(null, "Cancelar Pedido \n"
+						+ "Seleccione pedido");
+				break;
+			case 3:
+				JOptionPane.showMessageDialog(null, "Salir");
+				break;
+			default:
+				break;
+			}
+		} while (op!=3);
 	}
 
 }
