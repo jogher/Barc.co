5d81a9257f7b78d55687224f18ba0f4b9e27011d
Tomas De Paulo <tomas.depaulo@davinci.edu.ar>
2023-09-28 17:09:22 -0300

    Updates

--- src/interfaz/PantallaAdministracion.java
+++ src/interfaz/PantallaAdministracion.java
@@ -7,23 +7,20 @@ public class PantallaAdministracion {
 	
 		String[] Opciones=
 			{
-					"","Seguimiento de pedidos","Cancelar pedido","Salir"
+					"","","Salir"
 			};
 		int op=0;
 		do {
-			op= JOptionPane.showOptionDialog(null, "Menu Pedidos", null, 0, 0, null, Opciones, Opciones[0]);
+			op= JOptionPane.showOptionDialog(null, "Menu Administracion", null, 0, 0, null, Opciones, Opciones[0]);
 			switch (op) {
 			case 0:
-				JOptionPane.showMessageDialog(null, "Realizar pedido \n"
-						+ "Se debe ingresar(Productos)");
+				
 				break;
 			case 1:
-				JOptionPane.showMessageDialog(null, "Seguimiento de Pedidos \n"
-						+ "Seleccione un pedido");
+				
 				break;
 			case 2:
-				JOptionPane.showMessageDialog(null, "Cancelar Pedido \n"
-						+ "Seleccione pedido");
+				
 				break;
 			case 3:
 				JOptionPane.showMessageDialog(null, "Salir");
