ec7129318f409cc5c33c32102a4f7fb0012533ef
Tomas De Paulo <tomas.depaulo@davinci.edu.ar>
2023-09-28 17:03:08 -0300

    modifique el menu del main, y agregue lo del pedido en el menu de pedidos

---
+++ src/interfaz/PantallaAdministracion.java
@@ -0,0 +1,36 @@
+package interfaz;
+
+import javax.swing.JOptionPane;
+
+public class PantallaAdministracion {
+	public void Menu() { 
+	
+		String[] Opciones=
+			{
+					"","Seguimiento de pedidos","Cancelar pedido","Salir"
+			};
+		int op=0;
+		do {
+			op= JOptionPane.showOptionDialog(null, "Menu Pedidos", null, 0, 0, null, Opciones, Opciones[0]);
+			switch (op) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "Realizar pedido \n"
+						+ "Se debe ingresar(Productos)");
+				break;
+			case 1:
+				JOptionPane.showMessageDialog(null, "Seguimiento de Pedidos \n"
+						+ "Seleccione un pedido");
+				break;
+			case 2:
+				JOptionPane.showMessageDialog(null, "Cancelar Pedido \n"
+						+ "Seleccione pedido");
+				break;
+			case 3:
+				JOptionPane.showMessageDialog(null, "Salir");
+				break;
+			default:
+				break;
+			}
+		} while (op!=3);
+	}
+}
