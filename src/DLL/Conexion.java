d5e647f833cf7efca71bccbab3a57f0c1f7a0789
zarex97 <isaac.diaz@davinci.edu.ar>
2023-10-06 11:46:11 -0300

    feat: added login system
    añadido sistema de inicio de sesión, si no se tiene la contraseña no se puede continuar en el sistema, si no se quiere seguir intentando agregar la contraseña, se le da la opción al usuario de cerrar el programa.
    
    Adicionalmente fue creado el paquete DLL y la clase Conexión.

---
+++ src/DLL/Conexion.java
@@ -0,0 +1,25 @@
+package DLL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class Conexion {
+
+	private Connection con ;
+	
+	public Connection conectar() {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/barco","root","");
+			//JOptionPane.showMessageDialog(null, "se conecto");
+		} catch (Exception e) {
+	
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
